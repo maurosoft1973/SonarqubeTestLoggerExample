de7c68af89fcaa663dbf8a03581ef211433e1bc9
Mauro Cardillo <mauro.cardillo@gmail.com>
2024-08-04 15:54:09 +0200

    Create Calculator.cs

---
+++ src/SonarqubeTestLoggerExample/Calculator.cs
@@ -0,0 +1,9 @@
+namespace SonarqubeTestLoggerExample
+{
+    public class Calculator
+    {
+        public int Sum(int x, int y) => x + y;
+
+        public int Diff(int x, int y) => x - y;
+    }
+}
