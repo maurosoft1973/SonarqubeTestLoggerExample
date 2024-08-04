bdc8223c34939b819b4a7b64c15c0941a5d880ba
Mauro Cardillo <mauro.cardillo@gmail.com>
2024-08-04 15:54:21 +0200

    Delete src/SonarqubeTestLoggerExample/Class1.cs

--- src/SonarqubeTestLoggerExample/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SonarqubeTestLoggerExample
-{
-    public class Calculator
-    {
-        public int Sum(int x, int y) => x + y;
-
-        public int Diff(int x, int y) => x - y;
-    }
-}
